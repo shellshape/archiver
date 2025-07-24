5ca0f845deaf32099989b7b3b6ecd03509549ca3
zekroTJA <riho@live.de>
2025-07-24 22:28:36 +0200

    feat: add concurrent worker pool

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,13 @@ use crate::errors::Error;
 use anyhow::Result;
 use chrono::{DateTime, Datelike};
 use clap::{Parser, command};
-use indicatif::ProgressStyle;
+use indicatif::{ParallelProgressIterator, ProgressStyle};
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use std::fs::{self, DirEntry, File};
 use std::io;
+use std::num::NonZeroUsize;
 use std::path::{Path, PathBuf};
+use std::sync::Mutex;
 use std::time::UNIX_EPOCH;
 use yansi::Paint;
 
@@ -14,6 +17,10 @@ mod dirtree;
 mod errors;
 
 const PB_TEMPLATE: &str = "{spinner:.green} [{wide_bar:.cyan/dim}] {pos}/{len} ({per_sec}, {eta})";
+
+fn default_workers() -> NonZeroUsize {
+    NonZeroUsize::new(num_cpus::get()).expect("non zero cpu count")
+}
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -31,8 +38,12 @@ struct Cli {
     /// Overwrite existing files
     #[arg(short, long)]
     force: bool,
+
+    #[arg(short, long, default_value_t = default_workers())]
+    workers: NonZeroUsize,
 }
 
+#[derive(Debug)]
 enum Status {
     SkippedExists,
     SkippedIsDir,
@@ -42,7 +53,7 @@ enum Status {
 fn main() -> Result<()> {
     let cli = Cli::parse();
 
-    let mut dir_tree = DirTree::new(&cli.target_dir);
+    let dir_tree = Mutex::new(DirTree::new(&cli.target_dir));
 
     let files = fs::read_dir(cli.source_dir)
         .map_err(Error::ReadingSourceDir)?
@@ -53,30 +64,49 @@ fn main() -> Result<()> {
         .progress_chars("=>-");
     let bar = indicatif::ProgressBar::new(files.len() as u64).with_style(pb_style);
 
-    let mut failed = vec![];
-    let mut skipped = vec![];
+    // let mut failed = vec![];
+    // let mut skipped = vec![];
 
-    for entry in bar.wrap_iter(files.iter()) {
-        match process_entry(&mut dir_tree, entry, &cli.target_dir, cli.mv, cli.force) {
-            Ok(Status::Transferrred) => (),
-            Ok(Status::SkippedExists | Status::SkippedIsDir) => skipped.push(entry.file_name()),
-            Err(err) => failed.push((entry.file_name(), err)),
-        }
-    }
+    let worker_pool = rayon::ThreadPoolBuilder::new()
+        .num_threads(cli.workers.get())
+        .build()?;
+
+    let results: Vec<_> = worker_pool.install(|| {
+        files
+            .par_iter()
+            .progress_with(bar.clone())
+            .map(|entry| {
+                (
+                    entry,
+                    process_entry(&dir_tree, entry, &cli.target_dir, cli.mv, cli.force),
+                )
+            })
+            .collect()
+    });
 
     bar.finish_and_clear();
 
-    let n_processed = files.len() - failed.len() - skipped.len();
+    let skipped = results
+        .iter()
+        .filter(|(_, r)| matches!(r, Ok(Status::SkippedIsDir | Status::SkippedExists)))
+        .count();
+
+    let failed: Vec<_> = results
+        .iter()
+        .filter_map(|(e, r)| r.as_ref().err().map(|err| (e, err)))
+        .collect();
+
+    let n_processed = files.len() - failed.len() - skipped;
     println!(
         "{} {}",
         n_processed.to_string().green().bold(),
         "files have been processed.".green()
     );
 
-    if !skipped.is_empty() {
+    if skipped > 0 {
         println!(
             "{} {}",
-            skipped.len().to_string().yellow().bold(),
+            skipped.to_string().yellow().bold(),
             "files have been skipped.".yellow()
         );
     }
@@ -89,10 +119,10 @@ fn main() -> Result<()> {
             "files:".red()
         );
 
-        for (filename, err) in &failed {
+        for (dir_entry, err) in &failed {
             println!(
                 "{}",
-                format_args!(" - {}: {}", filename.to_string_lossy(), err).red()
+                format_args!(" - {}: {}", dir_entry.file_name().to_string_lossy(), err).red()
             );
         }
 
@@ -106,7 +136,7 @@ fn main() -> Result<()> {
 }
 
 fn process_entry(
-    dir_tree: &mut DirTree,
+    dir_tree: &Mutex<DirTree>,
     entry: &DirEntry,
     target_dir: impl AsRef<Path>,
     mv: bool,
@@ -133,6 +163,8 @@ fn process_entry(
         .join(format!("{:0>2}", mtime.day()));
 
     dir_tree
+        .lock()
+        .expect("mutex lock")
         .mkdir_all(&dir)
         .map_err(Error::CreatingTargetDirectory)?;
 
